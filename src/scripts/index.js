e42d2b8745d91ac846bf4e507868b1ec7b239cf7
RushiVachhani <vachhanirushi30@gmail.com>
2021-02-18 11:23:44 +0530

    finished movie page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -32,3 +32,54 @@ function getMovies(searchText) {
             console.log(err);
         })
 }
+
+function movieSelected(id) {
+    sessionStorage.setItem('movieid', id);
+    window.location = 'movie.html';
+    return false;
+}
+
+function getMovie() {
+    let movieId = sessionStorage.getItem('movieid');
+    console.log(movieId);
+    const url = `http://www.omdbapi.com/?i=${movieId}&apikey=${config.apiKey}`;
+    const imdbLink = `https://www.imdb.com/title/${movieId}/`;
+    axios.get(url)
+        .then((response) => {
+            let movie = response.data;
+            let output = `
+                <div class="row">
+                    <div clas="col-md-4">
+                        <img src="${movie.Poster}" class="thumbnail">
+                    </div>
+                    <div class="col-md-8>
+                        <h2>${movie.Title}</h2>
+                        <ul class="list-group">
+                            <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                            <li class="list-group-item"><strong>Released:</strong> ${movie.Release}</li>
+                            <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+                            <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+                            <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+                            <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+                            <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="well">
+                        <h3>Plot</h3>
+                        ${movie.Plot}
+                        <hr>
+                        <a href=${imdbLink} target="_blank" class="btn btn-primary">View IMDB</a>
+                        <a src"index.html" class="btn btn-default">Go back to Search</a>
+                    </div>
+                </div>
+            `;
+
+            $('#movie').html(output);
+
+        })
+        .catch((err)=> {
+            console.log(err);
+        })
+}
